da08c39cae2a07977baf66374f964c580717c968
Thales Ferreira <thales.martinho@hotmail.com>
2025-06-13 20:21:25 +0000

    adicione esse código tbm na parte mais alta, sem alterar ele <script>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,18 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.pixelId = "684c87d2684525761ce32bfa";
+              var a = document.createElement("script");
+              a.setAttribute("async", "");
+              a.setAttribute("defer", "");
+              a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+              document.head.appendChild(a);
+            `,
+          }}
+        />
+        <script
           src="https://cdn.utmify.com.br/scripts/utms/latest.js"
           data-utmify-prevent-xcod-sck
           data-utmify-prevent-subids
