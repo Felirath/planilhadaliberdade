8d1dd6d396b422eb676780f2b423a8a5e223593f
Thales Ferreira <thales.martinho@hotmail.com>
2026-01-17 20:57:08 +0000

    remova todos os pixels e script do facebook da utmify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 
 import type {Metadata} from 'next';
-import Script from 'next/script'; // Importar o componente Script
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
@@ -26,29 +25,6 @@ export default function RootLayout({
       <body className="font-body antialiased" suppressHydrationWarning={true}>
         {children}
         <Toaster />
-
-        {/* Script UTMify latest.js com Next/Script */}
-        <Script
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          strategy="afterInteractive"
-          data-utmify-prevent-xcod-sck // Manter atributos data-*
-          data-utmify-prevent-subids
-        />
-
-        {/* Configuração do Pixel ID */}
-        <Script id="utmify-pixel-config" strategy="afterInteractive">
-          {`
-            if (typeof window !== 'undefined') {
-              window.pixelId = "684c87d2684525761ce32bfa";
-            }
-          `}
-        </Script>
-
-        {/* Script UTMify pixel.js com Next/Script, dependendo da configuração acima */}
-        <Script
-          src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
